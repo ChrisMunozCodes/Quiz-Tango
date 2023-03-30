9d7370e738388339cd58f164c67e64a0bc0aa4d1
Christopher Munoz <christopherm0507@gmail.com>
2023-03-30 15:51:28 -0400

    Set up the server, modules, and dependencies. Solving issue #8

---
+++ server.js
@@ -0,0 +1,22 @@
+//path module
+const path = require('path');
+
+//creating express server
+const express = require("express");
+//calling the .env environment
+const dotenv = require("dotenv").config();
+//getting port value from .env or setting to 5000
+const port = process.env.PORT || 5000;
+//itializing express
+const app = express();
+
+
+//Enable body parser
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+
+//Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//passing port to test server
+app.listen(port, () => console.log(`Server started on port ${port}`));
