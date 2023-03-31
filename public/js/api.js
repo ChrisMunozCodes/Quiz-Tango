6cc7ef0e14bf098cba31df5516769be87f121067
Christopher Munoz <christopherm0507@gmail.com>
2023-03-30 21:36:07 -0400

    Completes issue #18

--- public/js/api.js
+++ public/js/api.js
@@ -28,10 +28,10 @@ function shuffleArray(array) {
       answerArr = shuffleArray(answerArr);
 
       //puts answers in the dom.
-      document.querySelector('.choice1').innerHTML = answerArr[0]
-      document.querySelector('.choice2').innerHTML = answerArr[1]
-      document.querySelector('.choice3').innerHTML = answerArr[2]
-      document.querySelector('.choice4').innerHTML = answerArr[3]
+      document.querySelector('.choice1 p').innerHTML = answerArr[0]
+      document.querySelector('.choice2 p').innerHTML = answerArr[1]
+      document.querySelector('.choice3 p').innerHTML = answerArr[2]
+      document.querySelector('.choice4 p').innerHTML = answerArr[3]
     })
     .catch(err => {
       console.log(`error ${err}`)
@@ -39,3 +39,81 @@ function shuffleArray(array) {
 
 
 /**** End of API code ****/
+
+//Those code adds the class 'selected' to whichever button is clicked. This changes the button to white and tracks the users selected answer.
+const firstBtn = document.querySelector('.choice1 button');
+const secondBtn = document.querySelector('.choice2 button');
+const thirdBtn = document.querySelector('.choice3 button');
+const fourthBtn = document.querySelector('.choice4 button');
+
+if (firstBtn) {
+  firstBtn.addEventListener('click', function() {
+    this.classList.add('selected');
+    if (secondBtn) {
+      secondBtn.classList.remove('selected');
+    }
+    if (thirdBtn) {
+      thirdBtn.classList.remove('selected');
+    }
+    if (fourthBtn) {
+      fourthBtn.classList.remove('selected');
+    }
+  });
+}
+
+if (secondBtn) {
+  secondBtn.addEventListener('click', function() {
+    this.classList.add('selected');
+    if (firstBtn) {
+      firstBtn.classList.remove('selected');
+    }
+    if (thirdBtn) {
+      thirdBtn.classList.remove('selected');
+    }
+    if (fourthBtn) {
+      fourthBtn.classList.remove('selected');
+    }
+  });
+}
+
+if (thirdBtn) {
+  thirdBtn.addEventListener('click', function() {
+    this.classList.add('selected');
+    if (firstBtn) {
+      firstBtn.classList.remove('selected');
+    }
+    if (secondBtn) {
+      secondBtn.classList.remove('selected');
+    }
+    if (fourthBtn) {
+      fourthBtn.classList.remove('selected');
+    }
+  });
+}
+
+if (fourthBtn) {
+  fourthBtn.addEventListener('click', function() {
+    this.classList.add('selected');
+    if (firstBtn) {
+      firstBtn.classList.remove('selected');
+    }
+    if (secondBtn) {
+      secondBtn.classList.remove('selected');
+    }
+    if (thirdBtn) {
+      thirdBtn.classList.remove('selected');
+    }
+  });
+}
+
+//** Submit button, click style ** //
+const submitButton = document.querySelector('.submit button');
+if (submitButton) {
+    submitButton.addEventListener('click', () => {
+        submitButton.classList.add('clicked');
+
+    setTimeout(() => {
+        submitButton.classList.remove('clicked');
+    }, 200);
+  });
+}
